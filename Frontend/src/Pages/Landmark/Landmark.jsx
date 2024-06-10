c95e2d27778a9d38dfe8185df4489ef293ecc40e
Omar Ahmed <omarahmed19991@outlook.com>
2024-06-10 15:06:59 +0300

    -Add a landmark page that view details about each landmark

--- Frontend/src/Pages/Landmark/Landmark.jsx
+++ Frontend/src/Pages/Landmark/Landmark.jsx
@@ -1,14 +1,94 @@
-import React from 'react'
-import './landmark.css'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import "./landmark.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Context } from "../../context/Context";
+import { assets } from "../../assets/assets";
 
 const Landmark = () => {
-
   const { landmark } = useParams();
+  const { backend_url, scrollLeft, scrollRight, ref } = useContext(Context);
+  const [landmarks, setLandmarks] = useState([]);
+  const NormalizeLandmark = landmark.replace(/-/g, " ");
+
+  let Filtered_Landmark;
+
+  const fetchLandmark = async () => {
+    try {
+      const response = await axios.get(backend_url + "/api/landmarks");
+      setLandmarks(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLandmark();
+  }, []);
+
+  Filtered_Landmark = landmarks.filter((item) =>
+    item.name.includes(NormalizeLandmark)
+  );
+
+  console.log(Filtered_Landmark);
 
   return (
-    <div>Landmark</div>
-  )
-}
+    <div className="landmark-container">
+      <div className="landmark-link">
+        <p>
+          <a href="/">Home</a> / <a href="/landmarks">Landmarks</a> /{" "}
+          {NormalizeLandmark}
+        </p>
+      </div>
+      {Filtered_Landmark.map((item) => (
+        <>
+          <div className="landmark-header">
+            <img src={backend_url + "/images/" + item.image[0]} alt="" />
+            <h1>{item.name}</h1>
+            <p>{item.country}</p>
+          </div>
+          <div className="landmark-details">
+            <div className="city">
+              <h2>City</h2>
+              <p>{item.city}</p>
+            </div>
+            <div className="zipcode">
+              <h2>Zipcode</h2>
+              <p>{item.zipcode}</p>
+            </div>
+            <div className="location">
+              <h2>Location</h2>
+              <p>{`lat: ${item.location.lat}, lon: ${item.location.lon}`}</p>
+            </div>
+          </div>
+          <div className="landmark-desc">
+            <div className="landmark-title">
+              <h1>Historical significance</h1>
+            </div>
+            <div className="landmark-para">
+              <div className="landmark-left">
+                <p>{item.desc}</p>
+              </div>
+              <div className="landmark-right">
+                <img src={backend_url + "/images/" + item.image[1]} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="landmark-gallery">
+            <div className="gallery-scroll-button">
+                <button onClick={scrollLeft}><img src={assets.arrow_left_scroll} alt="" /></button>
+                <button onClick={scrollRight}><img src={assets.arrow_right_scroll} alt="" /></button>
+            </div>
+              <div className="gallery-images" ref={ref}>
+                  {item.image.map((image) => (
+                      <img key={item._id} src={`${backend_url}/images/${image}`} alt="" />
+                  ))}
+              </div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
 
-export default Landmark
+export default Landmark;
--- Frontend/src/Pages/Nations/Nation.jsx
+++ Frontend/src/Pages/Nations/Nation.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './nation.css'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
